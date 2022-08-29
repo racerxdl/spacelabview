d9c76249d20e9241c78c51b1e8303b7501972744
Lucas Teske <lucas@teske.com.br>
2022-08-29 20:39:11 -0300

    Improved viewer

--- spacelab/voxel.go
+++ spacelab/voxel.go
@@ -1,9 +1,11 @@
 package spacelab
 
 type Voxel struct {
-	Id                  string
-	Name                string
-	DebugName           string
-	X, Y, Z             float64
-	SizeX, SizeY, SizeZ float64
+	Id                 string
+	Name               string
+	DebugName          string
+	X, Y, Z            float64
+	Size               float64
+	HasAtmosphere      bool
+	AtmosphereAltitude float64
 }
